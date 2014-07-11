a9b3c73d07496d32ee7c9686c660c3b1505073f0
Javier Provecho <j.provecho@dartekstudios.com>
2014-07-11 12:34:05 +0200

    Update example.go
    Edited to show more clearly that the cache works.

--- cache/example/example.go
+++ cache/example/example.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/contrib/cache"
+	"fmt"
 	"time"
 )
 
@@ -13,11 +14,11 @@ func main() {
 	store := cache.NewInMemoryStore(time.Second)
 	// Cached Page
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(200, "pong " + fmt.Sprint(time.Now().Unix()))
 	})
 
 	r.GET("/cache_ping", cache.CachePage(store, time.Minute, func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(200, "pong " + fmt.Sprint(time.Now().Unix()))
 	}))
 
 	// Listen and Server in 0.0.0.0:8080
