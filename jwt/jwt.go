6905bb63c5f6b2a04006f47f0961f05a8d6dd8cc
Javier Provecho Fernandez <javiertitan@gmail.com>
2015-01-03 14:12:29 +0100

    Fix JWT middleware and remove a custom test

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,14 +1,17 @@
 package jwt
 
 import (
+	"bytes"
 	jwt_lib "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(secret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) ([]byte, error) {
-			return []byte(secret), nil
+		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+			var b bytes.Buffer
+			b.Write([]byte(secret))
+			return b, nil
 		})
 
 		if err != nil {
