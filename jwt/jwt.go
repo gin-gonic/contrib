5d7f638dd36f1629f57f9935bd15696efe63bf08
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2015-03-30 23:11:33 +0200

    security fix unatohrized tokens could be used to access a protected ressource.

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -2,13 +2,15 @@ package jwt
 
 import (
 	"bytes"
+	"net/http"
+
 	jwt_lib "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(secret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+		token, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
 			var b bytes.Buffer
 			b.Write([]byte(secret))
 			return b, nil
@@ -16,6 +18,9 @@ func Auth(secret string) gin.HandlerFunc {
 
 		if err != nil {
 			c.Fail(401, err)
+		}
+		if !token.Valid {
+			http.Error(c.Writer, "Invalid Token", http.StatusUnauthorized)
 		}
 	}
 }
