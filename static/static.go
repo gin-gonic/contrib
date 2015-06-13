915fc6822dbf9137dafbd7f65cd7a58f5260d35b
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2015-06-13 11:52:29 +0200

    Improves static middleware

--- static/static.go
+++ static/static.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -15,77 +14,49 @@ type ServeFileSystem interface {
 	Exists(prefix string, path string) bool
 }
 
-func existsFile(name string) bool {
-	_, err := os.Stat(name)
-	return !os.IsNotExist(err)
-}
-
 type localFileSystem struct {
-	fs      http.FileSystem
+	http.FileSystem
 	root    string
 	indexes bool
 }
 
 func LocalFile(root string, indexes bool) *localFileSystem {
-	root, err := filepath.Abs(root)
-	if err != nil {
-		panic(err)
-	}
-
-	fs := http.Dir(root)
 	return &localFileSystem{
-		fs,
-		root,
-		indexes,
-	}
-}
-
-func (l *localFileSystem) Open(name string) (http.File, error) {
-	f, err := l.fs.Open(name)
-	if err != nil {
-		return nil, err
-	}
-
-	if l.indexes {
-		return f, err
-	} else {
-		return neuteredReaddirFile{f}, nil
+		FileSystem: gin.Dir(root, indexes),
+		root:       root,
+		indexes:    indexes,
 	}
 }
 
 func (l *localFileSystem) Exists(prefix string, filepath string) bool {
-
 	if p := strings.TrimPrefix(filepath, prefix); len(p) < len(filepath) {
-		p = path.Join(l.root, p)
-		return existsFile(p)
+		name := path.Join(l.root, p)
+		stats, err := os.Stat(name)
+		if err != nil {
+			return false
+		}
+		if !l.indexes && stats.IsDir() {
+			return false
+		}
+		return true
 	}
 	return false
 }
 
-type neuteredReaddirFile struct {
-	http.File
-}
-
-func (f neuteredReaddirFile) Readdir(count int) ([]os.FileInfo, error) {
-	return nil, nil
+func ServeRoot(urlPrefix, root string) gin.HandlerFunc {
+	return Serve(urlPrefix, LocalFile(root, false))
 }
 
 // Static returns a middleware handler that serves static files in the given directory.
-func Serve(prefix string, fs ServeFileSystem) gin.HandlerFunc {
-	var fileserver http.Handler
-
-	if prefix != "" {
-		fileserver = http.StripPrefix(prefix, http.FileServer(fs))
-	} else {
-		fileserver = http.FileServer(fs)
+func Serve(urlPrefix string, fs ServeFileSystem) gin.HandlerFunc {
+	fileserver := http.FileServer(fs)
+	if urlPrefix != "" {
+		fileserver = http.StripPrefix(urlPrefix, fileserver)
 	}
-
 	return func(c *gin.Context) {
-
-		if fs.Exists(prefix, c.Request.URL.Path) {
+		if fs.Exists(urlPrefix, c.Request.URL.Path) {
 			fileserver.ServeHTTP(c.Writer, c.Request)
-		} else {
-			c.Next()
+			c.Abort()
 		}
 	}
 }
