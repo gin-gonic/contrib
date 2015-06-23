1f05a3a345a3ae17599534244da228bc7ee8fcdf
Noah Shibley <archive.of.receipts@gmail.com>
2015-06-23 13:40:51 +0900

    added algo validation
    A vulnerability in JWT was recently discovered that allows access if the algorithm is not checked when the token is verified. This fixes it by following the recommendations of dgrijalva/jwt-go

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,13 +1,29 @@
 package jwt
 
 import (
+	"fmt"
+
 	jwt_lib "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-func Auth(secret string) gin.HandlerFunc {
+func Auth(secret string, alg string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+		token, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+
+			switch alg {
+			case "HS256", "HS384", "HS512":
+				if _, ok := token.Method.(*jwt_lib.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+			case "RSA256", "RSA384", "RSA512":
+				if _, ok := token.Method.(*jwt_lib.SigningMethodRSA); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+			default:
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
 			b := ([]byte(secret))
 			return b, nil
 		})
@@ -15,5 +31,9 @@ func Auth(secret string) gin.HandlerFunc {
 		if err != nil {
 			c.AbortWithError(401, err)
 		}
+		if !token.Valid {
+			c.Fail(401, errors.New("Invalid Token"))
+		}
 	}
 }
+
