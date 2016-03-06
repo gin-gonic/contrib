21b3cdead53c1ebbb5cd64ba0a4b37c724cc4697
Bertan Gundogdu <r.bertan.gundogdu@gmail.com>
2016-03-06 12:44:44 +0200

    Set decoded token claims into context

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -7,10 +7,12 @@ import (
 
 func Auth(secret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+		token, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
 			b := ([]byte(secret))
 			return b, nil
 		})
+
+		c.Set("token-claims", token.Claims)
 
 		if err != nil {
 			c.AbortWithError(401, err)
