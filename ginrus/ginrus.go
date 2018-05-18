c1c50dd08443a23b989fb3799eefc3aab4345880
Nathan J. Mehl <n@oden.io>
2018-05-18 17:01:48 -0400

    Add path skipping option to ginrus
    This brings ginrus.Ginrus into feature parity with
    gin.LoggerWithWriter()

--- ginrus/ginrus.go
+++ ginrus/ginrus.go
@@ -22,34 +22,45 @@ type loggerEntryWithFields interface {
 // It receives:
 //   1. A time package format string (e.g. time.RFC3339).
 //   2. A boolean stating whether to use UTC time zone or local.
-func Ginrus(logger loggerEntryWithFields, timeFormat string, utc bool) gin.HandlerFunc {
+//   3. Optionally, paths to skip logging for.
+func Ginrus(logger loggerEntryWithFields, timeFormat string, utc bool, notlogged ...string) gin.HandlerFunc {
+	var skip map[string]struct{}
+	if length := len(notlogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+		for _, path := range notlogged {
+			skip[path] = struct{}{}
+		}
+	}
 	return func(c *gin.Context) {
 		start := time.Now()
 		// some evil middlewares modify this values
 		path := c.Request.URL.Path
 		c.Next()
 
-		end := time.Now()
-		latency := end.Sub(start)
-		if utc {
-			end = end.UTC()
-		}
+		// log only when path is not being skippd
+		if _, ok := skip[path]; !ok {
+			end := time.Now()
+			latency := end.Sub(start)
+			if utc {
+				end = end.UTC()
+			}
 
-		entry := logger.WithFields(logrus.Fields{
-			"status":     c.Writer.Status(),
-			"method":     c.Request.Method,
-			"path":       path,
-			"ip":         c.ClientIP(),
-			"latency":    latency,
-			"user-agent": c.Request.UserAgent(),
-			"time":       end.Format(timeFormat),
-		})
+			entry := logger.WithFields(logrus.Fields{
+				"status":     c.Writer.Status(),
+				"method":     c.Request.Method,
+				"path":       path,
+				"ip":         c.ClientIP(),
+				"latency":    latency,
+				"user-agent": c.Request.UserAgent(),
+				"time":       end.Format(timeFormat),
+			})
 
-		if len(c.Errors) > 0 {
-			// Append error field if this is an erroneous request.
-			entry.Error(c.Errors.String())
-		} else {
-			entry.Info()
+			if len(c.Errors) > 0 {
+				// Append error field if this is an erroneous request.
+				entry.Error(c.Errors.String())
+			} else {
+				entry.Info()
+			}
 		}
 	}
 }
