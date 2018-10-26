588645d398f96ce3433f4ce4c373c552833d8864
ductm54 <ductm54@gmail.com>
2018-10-26 17:39:58 +0700

    [skip ci] remove validation rule from signature header

--- httpsignatures/signatureheader.go
+++ httpsignatures/signatureheader.go
@@ -1,14 +1,8 @@
 package httpsignatures
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 )
 
 const (
@@ -22,76 +16,54 @@ const (
 type SignatureHeader struct {
 	keyID     KeyID
 	headers   []string
-	signature []byte
+	signature string
 	algorithm string
-	date      time.Time
-	digest    string
 }
 
 //NewSignatureHeader new instace of SignatureHeader
 func NewSignatureHeader(r *http.Request) (*SignatureHeader, error) {
-	keyID, signature, headers, algorithm, err := fromHTTPRequest(r)
+	keyID, signature, headers, algorithm, err := parseHTTPRequest(r)
 	if err != nil {
 		return nil, err
-	}
-
-	date, err := http.ParseTime(r.Header.Get("Date"))
-	if err != nil {
-		return nil, err
-	}
-	digest := r.Header.Get("Digest")
-	for _, h := range headers {
-		if h == "digest" {
-			httpDigest, err := calculateDigest(r)
-			if err != nil {
-				return nil, err
-			}
-			if digest != httpDigest {
-				return nil, ErrInvalidDigest
-			}
-			break
-		}
 	}
 	return &SignatureHeader{
 		keyID:     KeyID(keyID),
 		headers:   headers,
-		date:      date,
 		signature: signature,
 		algorithm: algorithm,
-		digest:    digest,
 	}, nil
 }
 
-func fromHTTPRequest(r *http.Request) (keyID string, signature []byte, headers []string, algorithm string, err error) {
+func parseHTTPRequest(r *http.Request) (keyID string, signature string, headers []string, algorithm string, err error) {
 	s, err := getSignatureString(r)
 	if err != nil {
 		return keyID, signature, headers, algorithm, err
 	}
-	return fromSignatureString(s)
+	return parseSignatureString(s)
 }
 
-func fromSignatureString(s string) (keyID string, signature []byte, headers []string, algorithm string, err error) {
-	sigStructs := strings.Split(s, ",")
-	for _, pair := range sigStructs {
-		key, val, err := keyValSplit(pair)
-		if err != nil {
-			return keyID, signature, headers, algorithm, err
-		}
-		switch key {
-		case "keyId":
-			keyID = val
-		case "signature":
-			data, err := base64.StdEncoding.DecodeString(val)
-			if err != nil {
-				return keyID, signature, headers, algorithm, err
-			}
-			signature = data
-		case "headers":
-			headers = strings.Split(val, " ")
-		case "algorithm":
-			algorithm = val
-		}
+func parseSignatureString(s string) (keyID string, signature string, headers []string, algorithm string, err error) {
+	p := newParser(s)
+	results, err := p.parse()
+	if err != nil {
+		return keyID, signature, headers, algorithm, err
 	}
+	keyID, ok := results["keyId"]
+	if !ok {
+		return keyID, signature, headers, algorithm, ErrMissingKeyID
+	}
+	signature, ok = results["signature"]
+	if !ok {
+		return keyID, signature, headers, algorithm, ErrMissingSignature
+	}
+	headerString, ok := results["headers"]
+	if !ok || len(headerString) == 0 {
+		headers = []string{"date"}
+	} else {
+		headers = strings.Split(headerString, " ")
+	}
+	algorithm, _ = results["algorithm"]
+
 	return keyID, signature, headers, algorithm, nil
 }
 
@@ -105,31 +77,4 @@ func getSignatureString(r *http.Request) (string, error) {
 		return s[0], nil
 	}
 	return "", ErrNoSignature
-}
-
-func keyValSplit(s string) (key string, val string, err error) {
-	stringList := strings.SplitN(s, "=", 2)
-	if len(stringList) < 2 {
-		return "", "", ErrSignatureFormat
-	}
-	key = stringList[0]
-	val = strings.Trim(stringList[1], "\"")
-	return key, val, nil
-}
-
-func calculateDigest(r *http.Request) (string, error) {
-	if r.Body == nil {
-		return "", nil
-	}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return "", err
-	}
-	r.Body.Close()
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
-	h := sha256.New()
-	h.Write(body)
-	digest := fmt.Sprintf("SHA-256=%s", base64.StdEncoding.EncodeToString(h.Sum(nil)))
-	return digest, nil
 }
--- httpsignatures/signatureheader_test.go
+++ httpsignatures/signatureheader_test.go
@@ -1,12 +1,10 @@
 package httpsignatures
 
 import (
-	"encoding/base64"
 	"fmt"
 	"net/http"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -17,8 +15,6 @@ const (
 	sampleAlgorithm        = "rsa-sha256"
 	sampleSignature        = "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="
 	sampleBodyContent      = "hello world"
-	sampleDigest           = "SHA-256=uU0nuZNNPgilLlLX2n2r+sSE7+N6U4DukIj3rOLvzek="
-	sampleDigestNil        = ""
 	invalidSignatureString = "Signature keyId=\"rsa-key-1\",algorithm,headers=\"(request-target) host date digest \",signature=\"Hello world\""
 )
 
@@ -31,6 +27,13 @@ func newAuthorizationHeader(s string) http.Header {
 		`Authorization`: []string{s},
 	}
 }
+
+func newSignatureHeader(s string) http.Header {
+	return http.Header{
+		`Signature`: []string{s},
+	}
+}
+
 func TestFromSignatureString(t *testing.T) {
 	var tests = []struct {
 		name      string
@@ -47,32 +50,27 @@ func TestFromSignatureString(t *testing.T) {
 			err:    ErrNoSignature,
 		},
 		{
-			name:   `Signature invalid begin`,
+			name:   `Authorization Signature invalid begin`,
 			header: newAuthorizationHeader(`notASignature keyId="sample_key_id",algorithm="hmac-sha512",headers="(request-target) date digest",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
 			err:    ErrInvalidAuthorizationHeader,
 		},
 		{
-			name:   `Signature invalid key pair format`,
+			name:   `Authorization Signature invalid key pair format`,
 			header: newAuthorizationHeader(`Signature xxx`),
-			err:    ErrSignatureFormat,
+			err:    ErrUnterminatedParameter,
 		},
 		{
-			name:   `Signature missing keyId`,
+			name:   `Authorization Signature missing keyId`,
 			header: newAuthorizationHeader(`Signature algorithm="hmac-sha512",headers="",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
 			err:    ErrMissingKeyID,
 		},
 		{
-			name:   `Signature missing signature`,
+			name:   `Authorization Signature missing signature`,
 			header: newAuthorizationHeader(`Signature keyId="sample_key_id",algorithm="hmac-sha512",headers=""`),
 			err:    ErrMissingSignature,
 		},
 		{
-			name:   `Signature multiple space`,
-			header: newAuthorizationHeader(`Signature     keyId="sample_key_id",algorithm="hmac-sha512",headers="",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
-			err:    ErrInvalidAuthorizationHeader,
-		},
-		{
-			name:      `Signature header empty`,
+			name:      `Authorization Signature header empty`,
 			header:    newAuthorizationHeader(`Signature keyId="sample_key_id",algorithm="hmac-sha512",headers="",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
 			err:       nil,
 			keyID:     "sample_key_id",
@@ -81,7 +79,7 @@ func TestFromSignatureString(t *testing.T) {
 			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
 		},
 		{
-			name:      `Signature missing headers`,
+			name:      `Authorization Signature missing headers`,
 			header:    newAuthorizationHeader(`Signature keyId="sample_key_id",algorithm="hmac-sha512",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
 			err:       nil,
 			keyID:     "sample_key_id",
@@ -90,7 +88,7 @@ func TestFromSignatureString(t *testing.T) {
 			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
 		},
 		{
-			name:      `Normal case`,
+			name:      `Authorization Normal case`,
 			header:    newAuthorizationHeader(`Signature keyId="sample_key_id",algorithm="hmac-sha512",headers="(request-target) date digest",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
 			err:       nil,
 			keyID:     "sample_key_id",
@@ -99,7 +97,7 @@ func TestFromSignatureString(t *testing.T) {
 			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
 		},
 		{
-			name:      `Repeated params`,
+			name:      `Authorization Repeated params`,
 			header:    newAuthorizationHeader(`Signature keyId="sample_key_id",algorithm="hmac-sha512",headers="(request-target) date digest",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",keyId="sample_key_id_2"`),
 			err:       nil,
 			keyID:     "sample_key_id_2",
@@ -107,7 +105,52 @@ func TestFromSignatureString(t *testing.T) {
 			headers:   []string{"(request-target)", "date", "digest"},
 			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
 		},
-		//TODO: Test newSignatureHeader case
+		{
+			name:   `Signature missing keyId`,
+			header: newSignatureHeader(`algorithm="hmac-sha512",headers="",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
+			err:    ErrMissingKeyID,
+		},
+		{
+			name:   `Signature missing signature`,
+			header: newSignatureHeader(`keyId="sample_key_id",algorithm="hmac-sha512",headers=""`),
+			err:    ErrMissingSignature,
+		},
+		{
+			name:      `Signature header empty`,
+			header:    newSignatureHeader(`keyId="sample_key_id",algorithm="hmac-sha512",headers="",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
+			err:       nil,
+			keyID:     "sample_key_id",
+			algorithm: "hmac-sha512",
+			headers:   []string{"date"},
+			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
+		},
+		{
+			name:      `Signature missing headers`,
+			header:    newSignatureHeader(`keyId="sample_key_id",algorithm="hmac-sha512",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
+			err:       nil,
+			keyID:     "sample_key_id",
+			algorithm: "hmac-sha512",
+			headers:   []string{"date"},
+			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
+		},
+		{
+			name:      `Normal case`,
+			header:    newSignatureHeader(`keyId="sample_key_id",algorithm="hmac-sha512",headers="(request-target) date digest",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ=="`),
+			err:       nil,
+			keyID:     "sample_key_id",
+			algorithm: "hmac-sha512",
+			headers:   []string{"(request-target)", "date", "digest"},
+			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
+		},
+		{
+			name:      `Repeated params`,
+			header:    newSignatureHeader(`keyId="sample_key_id",algorithm="hmac-sha512",headers="(request-target) date digest",signature="70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",keyId="sample_key_id_2"`),
+			err:       nil,
+			keyID:     "sample_key_id_2",
+			algorithm: "hmac-sha512",
+			headers:   []string{"(request-target)", "date", "digest"},
+			signature: "70AaN3BDO0XC9QbtgksgCy2jJvmOvshq8VmjSthdXC+sgcgrKrl9WME4DbZv4W7UZKElvCemhDLHQ1Nln9GMkQ==",
+		},
 	}
 
 	for _, tc := range tests {
@@ -121,87 +164,9 @@ func TestFromSignatureString(t *testing.T) {
 		if err != nil {
 			continue
 		}
-		assert.Equal(t, tc.keyID, s.keyID, tc.name)
+		assert.Equal(t, KeyID(tc.keyID), s.keyID, tc.name)
 		assert.Equal(t, tc.algorithm, s.algorithm, tc.name)
 		assert.Equal(t, tc.headers, s.headers, tc.name)
-		assert.Equal(t, tc.signature, base64.StdEncoding.EncodeToString(s.signature), tc.name)
-
+		assert.Equal(t, tc.signature, s.signature, tc.name)
 	}
-}
-
-func TestCalculateDigest(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", strings.NewReader(sampleBodyContent))
-	digest, err := calculateDigest(r)
-	assert.Equal(t, sampleDigest, digest)
-	assert.Nil(t, err)
-}
-
-func TestCalculateDigestNilBody(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	digest, err := calculateDigest(r)
-	assert.Equal(t, sampleDigestNil, digest)
-	assert.Nil(t, err)
-}
-
-func TestNewSignatureHeaderNoSignature(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, s)
-	assert.Equal(t, err, ErrNoSignature)
-}
-
-func TestNewSignatureHeaderInvalidSignature(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	r.Header.Set(authorizationHeader, invalidSignatureString)
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, s)
-	assert.Equal(t, err, ErrSignatureFormat)
-}
-
-func TestNewSignatureHeaderDateMissing(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	r.Header.Set(authorizationHeader, sampleSignatureString)
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, s)
-	assert.NotNil(t, err)
-}
-
-func TestNewSignatureHeaderWithBodyWrongDigest(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", strings.NewReader(sampleBodyContent))
-	r.Header.Set(authorizationHeader, sampleSignatureString)
-	currentTime := time.Now()
-	r.Header.Set("Date", currentTime.Format(http.TimeFormat))
-	r.Header.Set("Digest", "this is wrong digest")
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, s)
-	assert.Equal(t, ErrInvalidDigest, err)
-}
-
-func TestNewSignatureHeaderNilBody(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	r.Header.Set(authorizationHeader, sampleSignatureString)
-	currentTime := time.Now()
-	r.Header.Set("Date", currentTime.Format(http.TimeFormat))
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, err)
-	assert.Equal(t, KeyID(sampleKeyID), s.keyID)
-	assert.Equal(t, sampleAlgorithm, s.algorithm)
-	assert.Equal(t, sampleHeader, s.headers)
-	assert.Equal(t, currentTime.Format(http.TimeFormat), s.date.Format(http.TimeFormat))
-	assert.Equal(t, sampleDigestNil, s.digest)
-}
-
-func TestNewSignatureHeaderWithBody(t *testing.T) {
-	r, _ := http.NewRequest("GET", "http://localhost", strings.NewReader(sampleBodyContent))
-	r.Header.Set(authorizationHeader, sampleSignatureString)
-	currentTime := time.Now()
-	r.Header.Set("Date", currentTime.Format(http.TimeFormat))
-	r.Header.Set("Digest", sampleDigest)
-	s, err := NewSignatureHeader(r)
-	assert.Nil(t, err)
-	assert.Equal(t, KeyID(sampleKeyID), s.keyID)
-	assert.Equal(t, sampleAlgorithm, s.algorithm)
-	assert.Equal(t, sampleHeader, s.headers)
-	assert.Equal(t, currentTime.Format(http.TimeFormat), s.date.Format(http.TimeFormat))
-	assert.Equal(t, sampleDigest, s.digest)
 }
